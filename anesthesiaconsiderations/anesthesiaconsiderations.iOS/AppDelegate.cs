765cabceb07a0fa233b4adbccd3ba1b431c20037
Eric Graham MacEachern <maharg@live.ca>
2017-05-03 09:34:25 -0700

    attempting to fix iOS version

--- anesthesiaconsiderations/anesthesiaconsiderations.iOS/AppDelegate.cs
+++ anesthesiaconsiderations/anesthesiaconsiderations.iOS/AppDelegate.cs
@@ -5,16 +5,13 @@ using System.Linq;
 using Foundation;
 using UIKit;
 
-using Xamarin.Forms;
-using Xamarin.Forms.Platform.iOS;
-
-namespace FormsGallery.iOS
+namespace anesthesiaconsiderations.iOS
 {
     // The UIApplicationDelegate for the application. This class is responsible for launching the 
     // User Interface of the application, as well as listening (and optionally responding) to 
     // application events from iOS.
     [Register("AppDelegate")]
-    public partial class AppDelegate : FormsApplicationDelegate
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
     {
         //
         // This method is invoked when the application has loaded and is ready to run. In this 
@@ -25,9 +22,9 @@ namespace FormsGallery.iOS
         //
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
-            Forms.Init();
-            Xamarin.FormsMaps.Init();
-            LoadApplication(new App());
+            global::Xamarin.Forms.Forms.Init();
+            LoadApplication(new anesthesiaconsiderations.App());
+
             return base.FinishedLaunching(app, options);
         }
     }
---
+++ anesthesiaconsiderations/anesthesiaconsiderations.iOS/anesthesiaconsiderations/App.cs
@@ -0,0 +1,8 @@
+﻿using Xamarin.Forms;
+
+namespace anesthesiaconsiderations
+{
+    internal class App : Application
+    {
+    }
+}
