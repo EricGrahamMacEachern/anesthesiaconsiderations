6252b9c8119ff1fac226ad90bc320d107ba7ef84
Eric Graham MacEachern <maharg@live.ca>
2017-05-07 16:28:53 -0700

    trying to fix runtime error
    Runtime error is preventing shipping.

--- anesthesiaconsiderations/anesthesiaconsiderations.iOS/Main.cs
+++ anesthesiaconsiderations/anesthesiaconsiderations.iOS/Main.cs
@@ -14,7 +14,7 @@ namespace anesthesiaconsiderations.iOS
 		{
 			// if you want to use a different Application Delegate class from "AppDelegate"
 			// you can specify it here.
-			UIApplication.Main(args, null, "AppDelegate");
+			 UIApplication.Main(args, null, "AppDelegate");
 		}
 	}
 }
--- anesthesiaconsiderations/anesthesiaconsiderations.iOS/anesthesiaconsiderations/App.cs
+++
@@ -1,8 +0,0 @@
-﻿using Xamarin.Forms;
-
-namespace anesthesiaconsiderations
-{
-    internal class App : Application
-    {
-    }
-}
