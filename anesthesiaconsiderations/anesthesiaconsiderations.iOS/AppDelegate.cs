0afcc7ff9864e814eaecec681190b9b93d1c5c6c
Eric Graham MacEachern <maharg@live.ca>
2017-05-08 21:29:12 -0700

    runtime error

--- anesthesiaconsiderations/anesthesiaconsiderations.iOS/AppDelegate.cs
+++ anesthesiaconsiderations/anesthesiaconsiderations.iOS/AppDelegate.cs
@@ -23,7 +23,7 @@ namespace anesthesiaconsiderations.iOS
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             global::Xamarin.Forms.Forms.Init();
-            LoadApplication(new anesthesiaconsiderations.App());
+            //LoadApplication(new anesthesiaconsiderations.App());
 
             return base.FinishedLaunching(app, options);
         }
